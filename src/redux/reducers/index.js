5c5578fd802785468dc748a6e16441e107b34105
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-04-08 11:34:54 +0300

    Added filter state value to persist blacklist

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,24 @@
 import { combineReducers } from 'redux';
 import themeReducer from './theme-reducer';
 import todoReducer from './todo-reducer';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['todos'],
+};
+
+const todosPersistConfig = {
+  key: 'todos',
+  storage,
+  blacklist: ['filter'],
+};
 
 const rootReducer = combineReducers({
+  todos: persistReducer(todosPersistConfig, todoReducer),
   theme: themeReducer,
-  todos: todoReducer,
 });
 
-export default rootReducer;
+export default persistReducer(rootPersistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import rootReducer from './reducers';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['theme', 'todos'],
-};
+import { persistStore } from 'redux-persist';
+import rootReducer from './reducers/index';
 
 const middlewares = [];
 
@@ -20,11 +13,6 @@ if (process.env.NODE_ENV === `development`) {
   middlewares.push(logger);
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = createStore(
-  persistedReducer,
-  applyMiddleware(...middlewares)
-);
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
