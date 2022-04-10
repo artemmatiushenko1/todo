0d41a235dfc884791e0084762a4637e2360a4763
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-04-10 18:06:21 +0300

    Chnaged regular image tag into SHA256 hash

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:lts-alpine
+FROM node@sha256:28bed508446db2ee028d08e76fb47b935defa26a84986ca050d2596ea67fd506
  
 WORKDIR /app
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,4 @@ import rootReducer from './reducers/index';
 
 export const store = createStore(rootReducer);
 
-// Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-//  recusandae repellat nihil tempore ad cum fugiat quasi aperiam tenetur
-//  omnis, alias, vel consequuntur est? Quaerat fugiat vero voluptates non asperiores.
-
 export const persistor = persistStore(store);
